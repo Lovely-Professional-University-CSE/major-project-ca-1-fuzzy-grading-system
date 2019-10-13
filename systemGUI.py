ff27b39f41d440bc7764d3b2d5f6b4d3bd0df0d5
rahulpsingh1 <31599528+rahulpsingh1@users.noreply.github.com>
2019-10-13 13:09:37 +0530

    update systemGUI.py
    added structure for login screen

--- systemGUI.py
+++ systemGUI.py
@@ -1,9 +1,26 @@
 
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
+window = Tk()
 window.title("Student Grading System")
+window.geometry("500x500")
 
-label = tk.Label(window, text = "hello world!").pack()
+global username
+global password
+username = StringVar()
+password = StringVar()
+
+Label(window, text = "Enter username and password below", bg="black",fg="green", font=("calibri", 11)).pack()
+Label(window, text = "").pack()
+
+Label(window, text = "Login Id ").pack()
+e1 = Entry(window, textvariable=username).pack()
+
+Label(window, text = "Password ").pack()
+e2 = Entry(window, textvariable=password, show='*').pack()
+
+Label(window, text="").pack()
+Button(window, text="Login", width=10, height=1).pack()
+
 
 window.mainloop()
