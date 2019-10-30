329a2f8c3fa8b0d2b668861f6baafae84532a0dd
lavanyakumarallu <42909214+lavanyakumarallu@users.noreply.github.com>
2019-10-30 11:15:04 +0530

    Improved code and fixed bugs

--- calculate_marks.py
+++ calculate_marks.py
@@ -1,80 +1,66 @@
 import numpy as np
 
-def atten(x):
+def atten(n):
     attny = []
-    for i in range(len(x)):
-        if (x[i] >= 90):
+    x = []
+    print("Enter attendence of each subject:")
+    for i in range(n):
+        j = eval(input('Enter attendence :'))
+        x.append(j)
+        if (j >= 90):
             attny.append(5)
-        elif (85<= x[i] <90):
+        elif (85<= j <90):
             attny.append(4)
-        elif (80<= x[i] <85):
+        elif (80<= j <85):
             attny.append(3)
-        elif (75<= x[i] <80):
+        elif (75<= j <80):
             attny.append(2)
         else:
             attny.append(1)
     attny = np.array(attny)
+    print(x)
     return attny
 
-def mte_mrks(mtex):
+def mte_mrks(n):
     mtey = []
-    for i in range(len(mtex)):
-        y = (mtex[i]/40)*20
+    print('Enter mte marks of Each subjet:')
+    mtex = []
+    for i in range(n):
+        j = eval(input('Enter mte subject marks:'))
+        mtex.append(j)
+        y = (j/40)*20
         mtey.append(round(y))
     mtey = np.array(mtey)
+    print(mtex)
     return mtey
 
-def ete_mrks(etex):
+def ete_mrks(n):
     etey = []
-    for i in range(len(etex)):
-        y = (etex[i]/70)*50
+    print('Enter ete marks of Each subjet:')
+    etex = []
+    for i in range(n):
+        j = eval(input('Enter ete subject marks:'))
+        etex.append(j)
+        y = (j/70)*50
         etey.append(round(y))
     etey = np.array(etey)
+    print(etex)
     return etey
 
-def ca_mrks(cax):
+def ca_mrks(n):
     cay = []
-    for i in range(len(cax)):
+    print('Enter CA marks of Each subjet:')
+    cax = []
+    for i in range(n):
+        print('Enter sbject',i+1,'CA marks:')
+        caxj = []
+        for j in range(3):
+            z = eval(input('Enter CA marks:'))
+            caxj.append(z)
+        caxj.remove(min(caxj))
+        cax.append(sum(caxj))
         y = (cax[i]/60)*25
         cay.append(round(y))
     cay = np.array(cay)
+    print(cax)
     return cay
-
-
-n = int(input('Enter Number of subjects:'))
-x = []
-print("Enter attendence of each subject:")
-for i in range(n):
-    j = eval(input('Enter attendence :'))
-    x.append(j)
-print(x)
-
-print('Enter mte marks of Each subjet:')
-mtex = []
-for i in range(n):
-    j = eval(input('Enter mte subject marks:'))
-    mtex.append(j)
-print(mtex)
-print('Enter ete marks of Each subjet:')
-etex = []
-for i in range(n):
-    j = eval(input('Enter ete subject marks:'))
-    etex.append(j)
-print(etex)
-
-print('Enter CA marks of Each subjet:')
-cax = []
-for i in range(n):
-    print('Enter sbject',i+1,'CA marks:')
-    caxj = []
-    for j in range(3):
-        z = eval(input('Enter CA marks:'))
-        caxj.append(z)
-    caxj.remove(min(caxj))
-    cax.append(sum(caxj))
-print(cax)
-
-print("Attendence Marks: ",atten(x))
-print("MTE Marks: ",mte_mrks(mtex))
-print("ETE Marks:  ",ete_mrks(etex))
-print("CA Marks: ",ca_mrks(cax))
